6ac703c880e6a3aa595f2e0443412afadc84af10
Max <mharris382@gmail.com>
2020-05-29 22:47:35 -0400

    Keyboard Input Works
    Test Input Scene
    Moves the "Player" object (red dot) using Unity's Input System. The Keyboard mapping works.

---
+++ Assets/PlayerMoveController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMoveController : MonoBehaviour
+{
+    // public InputAction moveAction;
+    public float moveSpeed = 10.0f;
+    public Vector2 position;
+
+    private Vector2 moveDirection = Vector2.zero;
+    
+    void Start()
+    {
+        // moveAction.Enable();
+        position = transform.position;
+    }
+
+     void Update()
+     {
+         // var moveDirection = moveAction.ReadValue<Vector2>();
+         position += moveDirection * moveSpeed * Time.deltaTime;
+         transform.position = position;
+     }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+}
