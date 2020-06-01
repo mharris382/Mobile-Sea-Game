9eea7fe66bef0ff5bad97b4866bc3078fcdb7ddd
Max <mharris382@gmail.com>
2020-06-01 15:21:03 -0400

    Eels follow player
    Eels and Player have a trigger collider. When collides, the eel's target becomes the player's transform until either the player leaves the eel's "detection circle" or the eel reaches their target

--- Assets/Scripts/Enemies/Eel.cs
+++ Assets/Scripts/Enemies/Eel.cs
@@ -1,119 +1,112 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Reflection;
 using UnityEngine;
 
-public class Eel : MonoBehaviour
+namespace enemies
 {
-    public float maxMoveSpeed = 2;
-    public float smoothing = 0.1f;
-    public float rotateSmoothing = 0.1f;
-    public Transform[] waypoints;
-
-
-    private int _index = 0;
-    private Transform _currentTarget;
-
-    private Vector3 _smoothDampVelocity; //variable used for unity's smooth damp method
-    private Vector3 _dampRotation;
-    private Waypoints _waypoints;
-
-    private void Awake() => this._waypoints = new Waypoints(this.waypoints, transform);
-
-
-    private void Update()
+    [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+    public class Eel : MonoBehaviour
     {
-        //should only be null when stopped chasing a target and waypoint hasn't been set yet
-        if (_currentTarget == null)
-            _currentTarget = this._waypoints.GetCurrentWaypoint();
+        public float maxMoveSpeed = 2;
+        public float smoothing = 0.1f;
+        public float rotateSmoothing = 0.1f;
+        public Transform[] waypoints;
 
 
+        private int _index = 0;
+        private Transform _currentTarget;
 
-        RotateTowardsTarget();
-        MoveTowardsTarget();
+        private Vector3 _smoothDampVelocity; //variable used for unity's smooth damp method
+        private Vector3 _dampRotation;
+        private Waypoints _waypoints;
 
-        if (WasTargetReached())
+
+        private void Awake()
         {
-            //Reset the target so that a new target will be found
-            _currentTarget = null;
+            this._waypoints = new Waypoints(this.waypoints, transform);
         }
-    }
 
-    private void MoveTowardsTarget()
-    {
-        var targetPos = _currentTarget.position;
-        var curPos = transform.position;
-        transform.position = Vector3.SmoothDamp(curPos, targetPos, ref _smoothDampVelocity, smoothing, maxMoveSpeed, Time.deltaTime);
-    }
 
-    private void RotateTowardsTarget()
-    {
-        //var cRot = transform.rotation;
-        //var tRot = Quaternion.LookRotation(_currentTarget.position, Vector3.forward);
-        //transform.rotation = Quaternion.Slerp(cRot, tRot, Time.deltaTime * rotateSpeed);
-        var curr = transform.forward;
-        var target = (_currentTarget.position - transform.position).normalized;
-        transform.forward = Vector3.SmoothDamp(curr, target, ref _dampRotation, rotateSmoothing);
-        
-    }
-
-    private bool WasTargetReached()
-    {
-        var dist = (_currentTarget.position - transform.position).sqrMagnitude;
-        return (dist < (0.125f * 0.125f));
-    }
-
-    [System.Obsolete("Moved functionality into waypoints")]
-    public void NextWp()
-    {
-        _index += 1;
-        _index %= waypoints.Length;
-    }
-
-    [System.Obsolete("Moved functionality into waypoints")]
-    private Transform GetCurrentWaypoint()
-    {
-        var wp = waypoints[_index];
-        _currentTarget = wp;
-        if (WasTargetReached())
+        private void Update()
         {
-            NextWp();
-            wp = waypoints[_index];
+            //should only be null when stopped chasing a target and waypoint hasn't been set yet
+            if (_currentTarget == null)
+                _currentTarget = this._waypoints.GetCurrentWaypoint();
+
+
+
+            RotateTowardsTarget();
+            MoveTowardsTarget();
+
+            if (WasTargetReached())
+            {
+                //Reset the target so that a new target will be found
+                _currentTarget = null;
+            }
         }
-        Debug.Assert(wp != null, "Waypoint is null", this);
-        return wp;
-    }
-}
 
-[System.Serializable]
-public class Waypoints
-{
-    public List<Transform> waypoints;
-    private Transform transform;
-
-    public Waypoints(Transform[] waypoints, Transform transform)
-    {
-        this.waypoints = new List<Transform>(waypoints);
-        this.transform = transform;
-
-    }
-
-    private int _index = 0;
-
-    private void NextWp()
-    {
-        _index += 1;
-        _index %= waypoints.Count;
-    }
-
-    public Transform GetCurrentWaypoint()
-    {
-        var wp = waypoints[_index];
-        var dist = (wp.position - transform.position).sqrMagnitude;
-        if ((dist < (0.125f * 0.125f)))
+        private void OnTriggerEnter2D(Collider2D other)
         {
-            NextWp();
-            wp = waypoints[_index];
+            if (other.CompareTag("Player") && !GameManager.Instance.IsPlayerHidden)
+            {
+                _currentTarget = other.transform;
+            }
         }
-        return wp;
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player") && (_currentTarget == other.transform))
+            {
+                _currentTarget = null;
+            }
+        }
+
+        private void MoveTowardsTarget()
+        {
+            var targetPos = _currentTarget.position;
+            var curPos = transform.position;
+            transform.position = Vector3.SmoothDamp(curPos, targetPos, ref _smoothDampVelocity, smoothing, maxMoveSpeed,
+                Time.deltaTime);
+        }
+
+        private void RotateTowardsTarget()
+        {
+            //var cRot = transform.rotation;
+            //var tRot = Quaternion.LookRotation(_currentTarget.position, Vector3.forward);
+            //transform.rotation = Quaternion.Slerp(cRot, tRot, Time.deltaTime * rotateSpeed);
+            var curr = transform.forward;
+            var target = (_currentTarget.position - transform.position).normalized;
+            transform.forward = Vector3.SmoothDamp(curr, target, ref _dampRotation, rotateSmoothing);
+
+        }
+
+        private bool WasTargetReached()
+        {
+            var dist = (_currentTarget.position - transform.position).sqrMagnitude;
+            return (dist < (0.125f * 0.125f));
+        }
+
+        [Obsolete("Moved functionality into waypoints")]
+        public void NextWp()
+        {
+            _index += 1;
+            _index %= waypoints.Length;
+        }
+
+        [Obsolete("Moved functionality into waypoints")]
+        private Transform GetCurrentWaypoint()
+        {
+            var wp = waypoints[_index];
+            _currentTarget = wp;
+            if (WasTargetReached())
+            {
+                NextWp();
+                wp = waypoints[_index];
+            }
+
+            Debug.Assert(wp != null, "Waypoint is null", this);
+            return wp;
+        }
     }
 }
---
+++ Assets/Scripts/Enemies/Waypoints.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace enemies
+{
+    [Serializable]
+    public class Waypoints
+    {
+        public List<Transform> waypoints;
+        private Transform transform;
+
+        public Waypoints(Transform[] waypoints, Transform transform)
+        {
+            this.waypoints = new List<Transform>(waypoints);
+            this.transform = transform;
+
+        }
+
+        private int _index = 0;
+
+        private void NextWp()
+        {
+            _index += 1;
+            _index %= waypoints.Count;
+        }
+
+        public Transform GetCurrentWaypoint()
+        {
+            var wp = waypoints[_index];
+            var dist = (wp.position - transform.position).sqrMagnitude;
+            if ((dist < (0.125f * 0.125f)))
+            {
+                NextWp();
+                wp = waypoints[_index];
+            }
+
+            return wp;
+        }
+    }
+}
