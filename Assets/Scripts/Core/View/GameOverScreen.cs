a245ee6b66f222f2ef9c03eced88c186064b82cd
Stephanie Jeandell <sljeandell@gmail.com>
2020-06-07 21:17:42 -0400

    reorganized scripts into proper folders, moved from Assets folder TODO: move into cooresponding namespaces

---
+++ Assets/Scripts/Core/View/GameOverScreen.cs
@@ -0,0 +1,53 @@
+﻿using Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI deathMessageText;
+    public TMPro.TextMeshProUGUI countdownTimerText;
+
+    [Tooltip("Additional delay before countdown timer begins.")]
+    public float extraDelay = 0;
+    [Tooltip("GameObject which will be toggled on GameOver.  Use to add a background to the GameOverScreen")]
+    public GameObject optionalFrame;
+
+    public void Awake()
+    {
+        GameManager.OnPlayerKilled += TriggerGameOver;
+        SetGameOverScreenEnabled(false);
+    }
+
+    public void OnDestroy() => GameManager.OnPlayerKilled -= TriggerGameOver;
+
+
+    private void TriggerGameOver(string message)
+    {
+        countdownTimerText.text = "";
+        deathMessageText.text = message;
+        StartCoroutine(DoCount());
+    }
+
+    private IEnumerator DoCount()
+    {
+        SetGameOverScreenEnabled(true);
+        yield return new WaitForSeconds(extraDelay);
+        for (int i = 3; i > 0; i--)
+        {
+            countdownTimerText.text = i.ToString();
+            yield return new WaitForSeconds(1);
+        }
+        SetGameOverScreenEnabled(false);
+        GameManager.Instance.ResetLevel();
+    }
+
+    private void SetGameOverScreenEnabled(bool v)
+    {
+        countdownTimerText.enabled = v;
+        deathMessageText.enabled = v;
+        if (optionalFrame != null)
+            optionalFrame.SetActive(v);
+    }
+
+}
---
+++ Assets/Scripts/Sub/SubController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubController : MonoBehaviour
+{
+
+    public Transform moveToTarget;
+
+    public float maxHorizontalSpeed = 15f;
+    public float maxDescentSpeed = 5f;
+    [Range(0,1)]
+    public float horizontalSmoothing = 0.1f;
+    [Range(0, 1)]
+    public float descentSmoothing = 0.5f;
+
+    //public float changeDirectionTime = 0.25f;
+
+
+    private bool _changingDirections;
+
+    private Vector3 _targetPostion;
+    private Vector3 _moveVelocity;
+
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _targetPostion = transform.position;
+    }
+
+    private void Update()
+    {
+        if(moveToTarget != null)
+        {
+            _targetPostion = moveToTarget.position;
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        var targetPos = _changingDirections ? (transform.position + _moveVelocity) : this._targetPostion;
+
+        var targetX = _targetPostion.x;
+        var curX = transform.position.x;
+        var xDir = curX - targetX;
+        CheckForDirectionChange(xDir);
+        var newX = Mathf.SmoothDamp(curX, targetX, ref _moveVelocity.x, horizontalSmoothing, maxHorizontalSpeed);
+
+        var curY = transform.position.y;
+        var targetY = _targetPostion.y;
+        var newY = Mathf.SmoothDamp(curY, targetY, ref _moveVelocity.y, descentSmoothing, maxDescentSpeed);
+
+        _rb.position = new Vector2(newX, newY);
+
+
+    }
+
+    private void CheckForDirectionChange(float xDir)
+    {
+        bool lastDir = _sr.flipX;
+        _sr.flipX = xDir > 0;
+        //if (_sr.flipX != lastDir)
+        //{
+        //    FlipDirections();
+        //}
+    }
+
+    //private void FlipDirections()
+    //{
+    //    _changingDirections = true;
+    //    Invoke("CompleteFlip", changeDirectionTime);
+    //}
+
+    //private void CompleteFlip()
+    //{
+    //    _changingDirections = false;
+    //}
+}
