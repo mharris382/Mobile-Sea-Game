b1d7cfb61511deb1abd74319a1e2daf70605f327
Max <mharris382@gmail.com>
2020-06-16 02:50:51 -0400

    Added Jellyfish and Sea Urchin
    added sea urchin sprite (64x64 at 32ppu)
    added jellyfish spritesheet (frames are 32x32 at 32ppu)
    added sea urchin and jellyfish code

---
+++ Assets/Scripts/Enemies/Jellyfish.cs
@@ -0,0 +1,40 @@
+﻿using Core;
+using UnityEngine;
+
+namespace enemies
+{
+    public class Jellyfish : MonoBehaviour
+    {
+        private const string DEATH_MESSAGE = "The diver got stung by a jellyfish!";
+        [Range(-1,1)]
+        public float safeZoneDot = 0;
+        
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+        
+            var hit = other.contacts[0];
+            if (!hit.collider.CompareTag("Player"))
+                return;
+                
+            var noraml = hit.normal;
+            var up = transform.up;
+            var dot = Vector2.Dot(noraml, up);
+            
+            if (dot > safeZoneDot)
+                return;
+            
+            
+            GameManager.Instance.KillDiver(DEATH_MESSAGE);
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                GameManager.Instance.KillDiver(DEATH_MESSAGE);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/UrchinHazard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Core;
+using UnityEngine;
+
+namespace enemies
+{
+    public class UrchinHazard : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                GameManager.Instance.KillDiver("The diver got pricked by a sea urchin!");
+            }
+        }
+    }
+}
