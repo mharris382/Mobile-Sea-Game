ba1d3482180f8a275e746b5f95e42cc16ecaa8ec
Stephanie Jeandell <sljeandell@gmail.com>
2020-06-23 01:26:49 -0400

    added utiltiy class that copies points from a polygon collider to a ShadowCaster2D removing the need to manually setup ShadowCasters in the scene.  The idea is to use a polygon colllider created from a sprite shape set to the lowest amount of detail.

---
+++ Assets/SpriteShapeShadowCaster.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class SpriteShapeShadowCaster : MonoBehaviour
+{
+    private ShadowCaster2D shadowCaster;
+    private PolygonCollider2D pc;
+
+    public bool autoGenerate = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shadowCaster = GetComponent<ShadowCaster2D>();
+        pc = GetComponent<PolygonCollider2D>();
+        SetShapePathFromCollider();
+    }
+
+    private void SetShapePathFromCollider()
+    {
+        var prop = shadowCaster.GetType().GetField("m_ShapePath", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var lop = new List<Vector3>();
+        lop.AddRange(pc.points.Select(t => (Vector3)t));
+        prop.SetValue(shadowCaster, lop.ToArray());
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(autoGenerate)
+        {
+            if(shadowCaster==null) shadowCaster = GetComponent<ShadowCaster2D>();
+            if(pc == null) pc = GetComponent<PolygonCollider2D>();
+            SetShapePathFromCollider();
+        }
+    }
+}
