456343b305a23301b8a494255a7c8b2a165e008b
Stephanie Jeandell <sljeandell@gmail.com>
2020-06-23 01:23:48 -0400

    Added some menu UI classes, just to start getting that out of the way so we don't need to do it all at the end

---
+++ Assets/Scripts/UI/MenuWindow.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MenuWindow : MonoBehaviour
+    {
+        [Tooltip("The window that will be opened when this one is closed")]
+        public MenuWindow onBackWindow;
+        public RectTransform menuRoot;
+        public GameObject defaultSelection;
+
+
+        private GameObject _lastSelection;
+        private bool _isOpen;
+
+        private static Queue<MenuWindow> _windowQueue = new Queue<MenuWindow>();
+
+        private void Awake()
+        {
+            _isOpen = false;
+            menuRoot.localPosition = Vector3.zero;
+            CloseWindow();
+        }
+
+        public void OnCloseAllWindowsButton(InputAction.CallbackContext context)
+        {
+            ChangeToWindow(null);
+        }
+
+        public void OnOpenButtonPressed(InputAction.CallbackContext context)
+        {
+            if (_isOpen) return;
+            ChangeToWindow(this);
+        }
+
+        public void OnBackPressed(InputAction.CallbackContext context)
+        {
+            if (_isOpen == false) return;
+            ChangeToWindow(onBackWindow);
+        }
+
+
+        private void CloseWindow()
+        {
+            _isOpen = false;
+            _lastSelection = EventSystem.current.currentSelectedGameObject ?? defaultSelection;
+            menuRoot.gameObject.SetActive(false);
+        }
+
+        private void OpenWindow()
+        {
+            _isOpen = true;
+            EventSystem.current.SetSelectedGameObject(_lastSelection ?? defaultSelection);
+            menuRoot.gameObject.SetActive(false);
+        }
+
+
+
+        public static void ChangeToWindow(MenuWindow window)
+        {
+            while(_windowQueue.Count > 0)
+            {
+                var nextWindow = _windowQueue.Dequeue();
+                //if window is already in queue for some reason
+                if (window != null && nextWindow == window)
+                {
+                    EnqeueAndOpen(window);
+                    return;
+                }
+                nextWindow.CloseWindow();
+            }
+            if(window != null) EnqeueAndOpen(window);
+        }
+
+        private static void EnqeueAndOpen(MenuWindow window)
+        {
+            _windowQueue.Enqueue(window);
+            window.OpenWindow();
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace UI
+{
+    [RequireComponent(typeof(Slider))]
+    public class VolumeSlider : MonoBehaviour
+    {
+        public AudioMixer audioMixer;
+        public string volumeParameterName = "Master Volume";
+
+        public float maxVolume = 10;
+        public float minVolume = -30;
+
+        public TMPro.TextMeshProUGUI percentLabel;
+
+        private Slider _slider;
+
+        public void Awake()
+        {
+            _slider = GetComponent<Slider>();
+            if (audioMixer == null) throw new UnityException($"Volume Slider {name} missing an AudioMixer");
+            float curVolume = 0;
+            if (!audioMixer.GetFloat(volumeParameterName, out curVolume))
+            {
+                Debug.LogError($"No Exposed Parameter named {volumeParameterName} in {audioMixer.name}", audioMixer);
+            }
+            _slider.minValue = 0;
+            _slider.maxValue = 1;
+            _slider.value = Mathf.InverseLerp(minVolume, maxVolume, curVolume);
+            
+            _slider.onValueChanged.AddListener(t =>
+            {
+                var newVolume = Mathf.Lerp(minVolume, maxVolume, t);
+
+                audioMixer.SetFloat(volumeParameterName, newVolume);
+            });
+
+
+        }
+    }
+
+}
