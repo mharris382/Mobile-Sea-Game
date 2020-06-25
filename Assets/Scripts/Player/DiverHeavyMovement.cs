a4a14fcf5c06366bd3aed5fc7876b3e0ff303916
Stephanie Jeandell <sljeandell@gmail.com>
2020-06-24 22:52:26 -0400

    implemented PlayerController with two states: carrying an object and SmoothDiverMovement as normal movement state

---
+++ Assets/Scripts/Player/DiverHeavyMovement.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using Core.State;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utilities;
+
+namespace Player
+{
+    public class DiverHeavyMovement : IState, IListenForMoveInput
+    { 
+        [System.Serializable]
+        public class Config
+        {
+            public float jumpForce = 1000;
+            public float moveSpeed = 10;
+            public float jumpResetTime = 0.5f;
+            public bool testing = true;
+        }
+        private readonly Rigidbody2D _rb;
+        private readonly Holder _objHolder;
+        private readonly Config _config;
+        private bool _canJump;
+        private float _xVelocity;
+        public DiverHeavyMovement(Rigidbody2D rb, Holder objHolder, Config config)
+        {
+            this._rb = rb;
+            _objHolder = objHolder;
+            _config = config;
+            _canJump = true;
+        }
+
+        public IEnumerator OnStateEnter()
+        {
+            _rb.isKinematic = false;
+            if (_config.testing) _rb.mass = 2.5f;
+            yield break;
+        }
+
+        public IEnumerator OnStateExit()
+        {
+            if (_config.testing) _rb.mass = 2f;
+            yield break;
+        }
+
+        public void Tick()
+        {
+        }
+
+        public void FixedTick()
+        {
+            var vel = _rb.velocity;
+            vel.x = _xVelocity;
+            _rb.velocity = vel;
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            var moveDir = context.ReadValue<Vector2>();
+            var dotUp = Vector2.Dot(moveDir, Vector2.up);
+            if (_canJump && dotUp > 0.5f)
+            {
+                _canJump = false;
+                _rb.AddForce(Vector2.up * _config.jumpForce, ForceMode2D.Impulse);
+                CoroutineHandler.instance.Invoke(() => _canJump = true , _config.jumpResetTime);
+            }
+            else if (_canJump && dotUp < -0.5f)
+            {
+                _rb.AddForce(Vector2.down * _config.jumpForce, ForceMode2D.Impulse);
+                _canJump = false;
+                CoroutineHandler.instance.Invoke(() => _canJump = true , _config.jumpResetTime);
+            }
+
+            _xVelocity = moveDir.x;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,94 @@
 ﻿using System;
 using Cinemachine;
 using Core;
+using Core.State;
 using Player.Diver;
 using Player.Sub;
 using PolyNav;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
         public DiverSmoothMovement diverMovement;
+        public DiverHeavyMovement.Config heavyMoveConfig;
+        public PlayerInput input;
+        [FormerlySerializedAs("isHoldingObject")] [SerializeField] bool _isHoldingObject;
+        
+        
+        private StateMachine _fsm;
 
+        public bool isHoldingObject
+        {
+            get => _isHoldingObject;
+            set => _isHoldingObject = value;
+        }
+
+        private void Awake()
+        {
+            _fsm = new StateMachine();
+            
+            var moveAction = input.actions["move"];
+            _fsm.OnStateChanged += (state, newState) =>
+            {
+                if (state is IListenForMoveInput)
+                {
+                    var prevListener = state as IListenForMoveInput;
+                    moveAction.performed -= prevListener.OnMove;
+                }
+
+                if (newState is IListenForMoveInput)
+                {
+                    var nextListener = newState as IListenForMoveInput;
+                    moveAction.performed += nextListener.OnMove;
+                }
+                else
+                {
+                    Debug.LogWarning("No State is listening for move input");
+                }
+            };
+
+            Holder objHolder = GetComponent<Holder>();
+            IState heavyMovement = new DiverHeavyMovement(diverMovement.GetComponent<Rigidbody2D>(), objHolder, heavyMoveConfig);
+            IState normalMovement = diverMovement;
+
+            _fsm.AddTransition(heavyMovement, normalMovement, () => _isHoldingObject == false);
+            _fsm.AddTransition(normalMovement, heavyMovement, () => _isHoldingObject == true);
+            _fsm.SetState(normalMovement);
+            moveAction.performed += (diverMovement as IListenForMoveInput).OnMove;
+        }
+
+
+        private void Update() => _fsm.Tick();
+
+        private void FixedUpdate() => _fsm.FixedTick();
+
+        private void LateUpdate() => ClampPositionToLevel();
+
+
+        private void ClampPositionToLevel( )
+        {
+            Vector3 position = diverMovement.rigidbody2D.position;
+            var rect = GameManager.Instance.CurrentLevel.GetLevelRect();
+
+            position.x = Mathf.Clamp(position.x, rect.xMin, rect.xMax);
+            position.y = Mathf.Clamp(position.y, rect.yMin, rect.yMax);
+            diverMovement.rigidbody2D.position = position;
+        }
+        
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            var moveDirection = context.ReadValue<Vector2>().normalized;
+           
+        }
+    }
+
+
+    public interface IListenForMoveInput
+    {
+        void OnMove(InputAction.CallbackContext context);
     }
 }
