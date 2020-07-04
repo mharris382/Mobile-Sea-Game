57d74f508648c4899ed6312f569589cf4c6b5680
Max <mharris382@gmail.com>
2020-07-03 20:23:32 -0400

    Reorganizing Files

---
+++ Assets/Scripts/Player/Diver/DiverHideEvents.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Player.Diver
+{
+    public class DiverHideEvents : MonoBehaviour
+    {
+        public UnityEvent OnDiverHidden;
+        public UnityEvent OnDiverUnhidden;
+        public bool onlyFireWhenEnabled = true;
+
+        private void Awake()
+        {
+            GameManager.OnPlayerHiddenChanged += isHidden =>
+            {
+                if (!enabled && onlyFireWhenEnabled)
+                    return;
+
+                if (isHidden)
+                {
+                    OnDiverHidden?.Invoke();
+                }
+                else
+                {
+                    OnDiverUnhidden?.Invoke();
+                }
+            };
+        }
+
+        private void Start()
+        {
+            OnDiverUnhidden?.Invoke();
+        }
+    }
+
+}
