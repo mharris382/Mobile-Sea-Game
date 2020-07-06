a495371993c7204a1e831cc86842933608042b9f
Max <mharris382@gmail.com>
2020-07-05 22:26:56 -0400

    Added class for PlayerControlledSettings and added fast move speed in config
    -PlayerControlledSettings uses PlayerPrefs to store player settings 
    -currently has one setting: whether or not fast movement is the default movement 
    (aka the movement speed when button is not held down)

--- Assets/Scripts/Player/Diver/DiverConfig.cs
+++ Assets/Scripts/Player/Diver/DiverConfig.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
 
 namespace Player.Diver
 {
@@ -6,8 +7,9 @@ namespace Player.Diver
     public class DiverConfig : ScriptableObject
     {
         [Min(0)] public float moveSpeed = 2;
-
-
+        
+        [MinValue(("moveSpeed"))]
+        public float fasterMoveSpeed = 10;
         [Tooltip("Depth at which flashlight will be always be enabled")]
         [SerializeField]private int flashlightDepth = 10;
 
@@ -32,8 +34,39 @@ namespace Player.Diver
         public static float FlashlightDepth => Instance. flashlightDepth;
 
         public static float MaxHookHoldDistance => Instance.maxHookHoldDistance;
+
+
+        
+        public static float GetDiverMoveSpeed(bool isFasterButtonHeld)
+        {
+            if (PlayerControlledSettings.GetDefaultIsFastSetting())
+            {
+                isFasterButtonHeld = !isFasterButtonHeld;
+            }
+
+            return isFasterButtonHeld ? Instance.fasterMoveSpeed : Instance.moveSpeed;
+        }
     }
-    
-    
+
+    public static class PlayerControlledSettings
+    {
+        private const string IS_FAST_MOVE_DEFAULT_PREF = "moveDefaultIsFast";
+
+
+
+        public static bool GetDefaultIsFastSetting()
+        {
+            if (!PlayerPrefs.HasKey(IS_FAST_MOVE_DEFAULT_PREF))
+            {
+                PlayerPrefs.SetInt(PlayerControlledSettings.IS_FAST_MOVE_DEFAULT_PREF, 0);
+            }
+            return  PlayerPrefs.GetInt(PlayerControlledSettings.IS_FAST_MOVE_DEFAULT_PREF) > 0;
+        }
+
+        public static void SetDefaultIsFastSetting(bool defaultIsFastMovement)
+        {
+            PlayerPrefs.SetInt(IS_FAST_MOVE_DEFAULT_PREF, defaultIsFastMovement ? 1 : 0);
+        }
+    }
     
 }
