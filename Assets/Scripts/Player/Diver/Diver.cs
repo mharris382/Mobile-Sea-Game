129bd5793fc989d479b559ba47803de54df25b13
Max <mharris382@gmail.com>
2020-07-08 00:42:44 -0400

    Diver implements IDiverMovement

--- Assets/Scripts/Player/Diver/Diver.cs
+++ Assets/Scripts/Player/Diver/Diver.cs
@@ -6,18 +6,21 @@ using DiverActions = UnderTheSeaInput.DiverGameplayActions;
 namespace Player.Diver
 {
     [RequireComponent(typeof(Rigidbody2D))]
-    public class Diver : MonoBehaviour
+    public class Diver : MonoBehaviour, IDiverMovement
     {
         private StateMachine _fsm;
         private DiverActions _diverActions;
         private Holder _holder;
         private Rigidbody2D _rigidbody2D;
         private ClampToLevel _positionConstraint;
-        
+        private Vector2 _moveDirection;
+
+
         private void Awake()
         {
             _fsm = new StateMachine();
             _diverActions = GameInput.DiverGameplayActions;
+            _diverActions.Move.performed += context => _moveDirection = context.ReadValue<Vector2>();
             _positionConstraint = new ClampToLevel();
             
             _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -49,5 +52,17 @@ namespace Player.Diver
         private void OnEnable() => _diverActions.Disable();
 
         private void OnDisable() => _diverActions.Disable();
+
+        public Rigidbody2D rigidbody2D
+        {
+            get => _rigidbody2D;
+            set => _rigidbody2D = value;
+        }
+
+        public Vector2 MoveDirection
+        {
+            get => _moveDirection;
+            set => _moveDirection = value;
+        }
     }
 }
