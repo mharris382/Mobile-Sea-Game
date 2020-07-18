0949784e0239102ee4c31ef08e6e216d08286cce
Max <mharris382@gmail.com>
2020-07-18 19:07:37 -0400

    Added Diver basic swim anims and controller
    - Basic swim  animations (swim and idle)
    - setup diver Animator controller
    -flipped using layers
    -created script that passes input into the animator

---
+++ Assets/Scripts/Player/Diver/DiverAnimatorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+[RequireComponent(typeof(Animator))]
+public class DiverAnimatorController : MonoBehaviour
+{
+    private Animator _anim;
+    private bool _faceLeft = true;
+
+    private string a_IsMoving = "IsMoving";
+    private string a_FaceLeft = "FaceLeft";
+    private string a_VelX = "VelocityX";
+    private string a_VelY = "VelocityY";
+    
+
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        var movement = context.ReadValue<Vector2>();
+        bool isMoving = movement != Vector2.zero;
+        _anim.SetBool(a_IsMoving, isMoving);
+        if(!isMoving)
+            return;
+        if (movement.x != 0)
+        {
+            _faceLeft = movement.x < 0;
+        }
+        _anim.SetBool(a_FaceLeft, _faceLeft);
+        _anim.SetFloat(a_VelX, movement.x);
+        _anim.SetFloat(a_VelY, movement.y);
+    }
+}
