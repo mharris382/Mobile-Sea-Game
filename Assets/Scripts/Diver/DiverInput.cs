f5c6d79862f43b801a91e1cbfb5f93dfe55d480a
Max <mharris382@gmail.com>
2020-08-12 21:40:36 -0400

    Created DiverInput class to act as a go-between for Unity Input System

---
+++ Assets/Scripts/Diver/DiverInput.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Diver
+{
+    public class DiverInput: MonoBehaviour
+    {
+        public bool useInputClassInstance = false;
+
+        [HideIf("useInputClassInstance"),Required] public PlayerInput playerInput;
+
+        private InputAction _moveAction;
+        private InputAction _moveHookAction;
+
+        public float HookMoveInput { private set; get; }
+        public Vector2 DiverMoveInput { private set; get; }
+        
+        private void Start()
+        {
+            InputAction interact=null, useHook=null, toggleSprint =null;
+            if (useInputClassInstance)
+            {
+                var m_controls = new UnderTheSeaInput();
+                throw new NotImplementedException();
+            }
+            else
+            {
+                _moveAction = GetActionSafe("Move");
+                _moveHookAction = GetActionSafe("MoveHook");
+                interact = GetActionSafe("Interact");
+                useHook = GetActionSafe("Hook");
+                toggleSprint = GetActionSafe("ToggleFastMove");
+            }
+
+            interact.started += context => { };
+        }
+
+        private InputAction GetActionSafe(string moveHookName)
+        {
+            var moveHookAction = playerInput.actions[moveHookName];
+            Debug.Assert(_moveHookAction != null, $"No Action named {moveHookName} found!");
+            return moveHookAction;
+        }
+
+
+        private void Update()
+        {
+            this.DiverMoveInput = _moveAction.ReadValue<Vector2>();
+            this.HookMoveInput = _moveHookAction.ReadValue<float>();
+            
+        }
+    }
+}
