7f1a4b54c80e82bbabe96756396643e23fa2285c
Max <mharris382@gmail.com>
2020-08-13 15:36:49 -0400

    added unirx extensions for InputAction events

---
+++ Assets/Scripts/Diver/UniRxInputSystemExtensions.cs
@@ -0,0 +1,29 @@
+﻿using UniRx;
+using UnityEngine.InputSystem;
+
+namespace Diver
+{
+    public static class UniRxInputSystemExtensions
+    {
+        public static IObservable<InputAction.CallbackContext> PerformedAsObservable(this InputAction action)
+        {
+            return Observable.FromEvent<InputAction.CallbackContext>(
+            t => action.performed += t, 
+            t => action.performed -= t);
+        }
+        
+        public static IObservable<InputAction.CallbackContext> StartedAsObservable(this InputAction action)
+        {
+            return Observable.FromEvent<InputAction.CallbackContext>(
+                t => action.started += t, 
+                t => action.started -= t);
+        }
+        
+        public static IObservable<InputAction.CallbackContext> CancelledAsObservable(this InputAction action)
+        {
+            return Observable.FromEvent<InputAction.CallbackContext>(
+                t => action.canceled += t, 
+                t => action.canceled -= t);
+        }
+    }
+}
