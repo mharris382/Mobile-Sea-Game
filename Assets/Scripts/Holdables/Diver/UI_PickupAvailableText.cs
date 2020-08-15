43c0f7cbd635adbf1f0b535aaff66f909261c57c
Max <mharris382@gmail.com>
2020-08-15 14:41:43 -0400

    Added a ui controller to show when holdable can be picked up

---
+++ Assets/Scripts/Holdables/Diver/UI_PickupAvailableText.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Holdables.Diver
+{
+    public class UI_PickupAvailableText : MonoBehaviour
+    {
+        public TMPro.TextMeshProUGUI notificationText;
+        
+        
+        private HoldableProvider _provider;
+        private Holder _holder;
+
+
+        [Inject]
+        void Inject(Holder holder, HoldableProvider provider)
+        {
+            this._holder = holder;
+            this._provider = provider;
+        }
+
+
+        private void Start()
+        {
+            Debug.Assert(_holder != null, "Null Holder injected", this);
+            Debug.Assert(_provider != null, "Null Provider Injected", this);
+            
+            notificationText.enabled = true;
+            notificationText.text = "";
+            transform.parent = null;
+        }
+
+        private void Update()
+        {
+            var available = _holder.HeldObject != null ? null : _provider.GetFirstChoiceForPickup();
+            if (available == null)
+            {
+                notificationText.text = "";
+            }
+            else
+            {
+                notificationText.text = $"Press E to pickup {available.name}";
+            }
+        }
+
+    }
+}
