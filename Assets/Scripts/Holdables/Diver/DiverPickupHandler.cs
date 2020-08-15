21806f725c0652cb69366407468a5a10b606edf7
Max <mharris382@gmail.com>
2020-08-15 14:44:53 -0400

    Created new pickup controllers for diver
    -new prefab installer: DiverInstaller which currently injects only the PlayerInput
    -added controller for handling pickup input (DiverPickupHandler)
    -added controller for handling pickup physics (DiverPickupJoints)
    -joints uses JointTemplates which are prefabs, added folder Prefabs/JointTemplates

---
+++ Assets/Scripts/Holdables/Diver/DiverPickupHandler.cs
@@ -0,0 +1,40 @@
+﻿using Diver;
+using Sirenix.OdinInspector;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace Holdables.Diver
+{
+    [TypeInfoBox("Listens for input and triggers pickups and drops")]
+    public class DiverPickupHandler : MonoBehaviour
+    {
+        private DiverInput _input;
+        private Holder _holder;
+        private HoldableProvider _provider;
+        
+        
+        [Inject]
+        void Inject(Holder diverHolder, HoldableProvider holdableProvider, DiverInput diverInput)
+        {
+            this._holder = diverHolder;
+            this._provider = holdableProvider;
+            this._input = diverInput;
+        }
+
+        private void Start()
+        {
+            _input.OnInteract.Where(t => _holder.HeldObject == null)
+                .Select(t => _provider.GetFirstChoiceForPickup()).
+                Where(t => t != null).
+                Subscribe(t => _holder.PickupObject((t)));
+
+            _input.OnInteract.Where(t => _holder.HeldObject != null)
+                .Subscribe(_ => _holder.ReleaseHeldObject());
+            
+            
+        }
+
+        
+    }
+}
