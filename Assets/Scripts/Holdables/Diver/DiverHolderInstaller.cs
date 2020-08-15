d004bbf21ae62b2d5221da17bdaea1125c2762b0
Max <mharris382@gmail.com>
2020-08-14 21:35:16 -0400

    Added a new heavy holdable class and new test scene
    -moved old heavy code from the installer class to its own file for cleanup

--- Assets/Scripts/Holdables/Diver/DiverHolderInstaller.cs
+++ Assets/Scripts/Holdables/Diver/DiverHolderInstaller.cs
@@ -1,4 +1,4 @@
-using JetBrains.Annotations;
+using System;
 using Player.Diver;
 using UniRx;
 using UnityEngine;
@@ -11,61 +11,24 @@ namespace Holdables.Diver
         public override void InstallBindings()
         {
             Container.Bind<Rigidbody2D>().FromComponentInHierarchy().AsSingle();
-            
+
             //NOTE: if it becomes necessary to inject the HoldableSelector this needs to be changed to BindToSelfAndInterfaces
             Container.Bind<Holder>().AsSingle();
 
             Container.DeclareSignal<DiverHeldItemChangedSignal>();
             Container.BindInterfacesTo<DiverHeldItemChangedSignal.Publisher>().AsSingle();
-            
-            
+           
+
             //TODO: Move to a different installer
             Container.Bind<MonoBehaviour>().To<DiverSmoothMovement>().FromComponentsInHierarchy().WhenInjectedInto<StopMovementOnHoldHeavy>();
             Container.Bind<StopMovementOnHoldHeavy>().AsSingle();
             Container.BindSignal<DiverHeldItemChangedSignal>().ToMethod<StopMovementOnHoldHeavy>(t => t.OnHeldItemChanged).FromResolve();
+
+            Container.BindInterfacesTo<HeavyHoldableListener>().AsSingle();
         }
 
         
+        
 
-        [UsedImplicitly]
-        public class StopMovementOnHoldHeavy
-        {
-            private readonly MonoBehaviour _movement;
-
-            private bool _holdingHeavyObject;
-
-            public StopMovementOnHoldHeavy(MonoBehaviour movement)
-            {
-                _movement = (MonoBehaviour) movement;
-            }
-
-            public void OnHeldItemChanged(DiverHeldItemChangedSignal signal)
-            {
-                if (signal.HeldObject == null)
-                {
-                    if (!_holdingHeavyObject)
-                        return;
-
-                    SetHoldingHeavy(false);
-                }
-                else if (IsPickupHeavy(signal))
-                {
-                    SetHoldingHeavy(true);
-                }
-            }
-
-            private bool IsPickupHeavy(DiverHeldItemChangedSignal signal)
-            {
-                if (signal.HeldObject.rigidbody2D == null) return false;
-                return signal.HeldObject.rigidbody2D.CompareTag("Heavy");
-                //return signal.HeldObject.rigidbody2D != null && signal.HeldObject.rigidbody2D.mass >= _weightLimit;
-            }
-
-            private void SetHoldingHeavy(bool isHoldingHeavy)
-            {
-                _holdingHeavyObject = isHoldingHeavy;
-                _movement.enabled = !isHoldingHeavy;
-            }
-        }
     }
 }
---
+++ Assets/Scripts/Holdables/Diver/HeavyHoldable.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Holdables.Diver
+{
+    [TypeInfoBox("Holdable that is too heavy for diver to swim with, but can still be carried horizontally")]
+    public class HeavyHoldable : MonoBehaviour
+    {
+        internal static HashSet<IHoldable> heavyHoldables = new HashSet<IHoldable>(); 
+        
+        private IHoldable _holdable;
+        private bool _isHeld;
+        
+        private void Awake()
+        {
+            this._holdable = GetComponent<IHoldable>();
+            Debug.Assert(_holdable!=null, "Missing Holdable", this);
+        }
+
+        private void OnEnable()
+        {
+            heavyHoldables.Add(_holdable);
+        }
+
+        private void OnDisable()
+        {
+            heavyHoldables.Remove(_holdable);
+        }
+    }
+}
